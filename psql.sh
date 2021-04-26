22700b13b0a200449aa90c5c8c93b164b7135b54
Mike Izbicki <mike@izbicki.me>
2021-04-26 09:04:43 -0700

    update hw

--- psql.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# load environment variables
-while read var; do
-    export $var
-done < .env.prod.db
-
-docker exec -it novichenko_pg_1 psql $POSTGRES_DB $POSTGRES_USER
-#docker exec -it novichenko_db_1 psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@db/$POSTGRES_DB"
